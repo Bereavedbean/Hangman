9fee80fc49daa4a2849309ef8a3acc2948fac089
Julian Moltere <56549721+Bereavedbean@users.noreply.github.com>
2022-03-20 19:00:11 -0400

    Update CStringFormatting.py

--- CStringFormatting.py
+++ CStringFormatting.py
@@ -1,5 +1,7 @@
 #this just formats strings into caracter arrays
 #note, you have to edit it to fix the added comma
+#made this because the first revision doesn't allow me to declare the words as strings :(
+#I initially declared them this way and made this to re-add all 50 words to guess in hangman
 def main():
 
     inputs = '1'
